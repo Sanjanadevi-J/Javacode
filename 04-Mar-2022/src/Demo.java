746ca971be299f2206edf3f65677b90a781814d2
Sanjanadevi-J <100769605+Sanjanadevi-J@users.noreply.github.com>
2022-03-04 15:11:46 +0530

    Static

---
+++ 04-Mar-2022/src/Demo.java
@@ -0,0 +1,15 @@
+class sum {
+     public static int summing(int a, int b)
+    {
+        return a+b;
+    }
+}
+public class Demo {
+    public static void main(String[]args)
+    {
+        int m=3,n=5;
+        int s =sum.summing(m,n);
+        System.out.println("sum is:"+s);
+    }
+
+}

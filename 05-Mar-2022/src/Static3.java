acfe8a31bd472efc5f1a6843d6852eb8c6c33bf0
Sanjanadevi-J <100769605+Sanjanadevi-J@users.noreply.github.com>
2022-03-05 13:55:39 +0530

    Static block

---
+++ 05-Mar-2022/src/Static3.java
@@ -0,0 +1,9 @@
+public class Static3 {
+    static {
+        System.out.println("Static block");
+    }
+    public static void main(String[]args)
+    {
+        System.out.println("Main method");
+    }
+}

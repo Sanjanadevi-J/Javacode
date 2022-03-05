80a9bad9fb6023129a83ac81f11de6d121012aa6
Sanjanadevi-J <100769605+Sanjanadevi-J@users.noreply.github.com>
2022-03-05 13:49:30 +0530

    Static1

---
+++ 05-Mar-2022/src/Static1.java
@@ -0,0 +1,11 @@
+public class Static1 {
+    public static void data()
+    {
+        System.out.println("Static method");
+
+    }
+    public static void main(String[]args)
+    {
+      Static1.data();
+    }
+}

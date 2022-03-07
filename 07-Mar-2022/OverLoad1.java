2d47d930b41f1c1f0020d8482095ec34081e70ef
Sanjanadevi-J <100769605+Sanjanadevi-J@users.noreply.github.com>
2022-03-07 15:59:08 +0530

    Differ by number of arguments

---
+++ 07-Mar-2022/OverLoad1.java
@@ -0,0 +1,13 @@
+public class OverLoad1 {
+    int add(int a,int b) {
+        return a+b;
+    }
+    int add(int a,int b,int c) {
+        return a+b+c;
+    }
+    public static void main(String[]args) {
+      OverLoad1 ol = new OverLoad1();
+      System.out.println("sum is"+ol.add(5,7);
+      System.out.println("sum is:"+ol.add(3,7,9);
+    }
+}

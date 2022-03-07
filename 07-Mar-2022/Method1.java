18f410453390b479786fb56cd5df3a8e3671d39b
Sanjanadevi-J <100769605+Sanjanadevi-J@users.noreply.github.com>
2022-03-07 13:03:09 +0530

    Predefined

---
+++ 07-Mar-2022/Method1.java
@@ -0,0 +1,7 @@
+public class Method1 {
+    String name= "Predefined method";
+    public static void main(String[]args) {
+        int length = name.length();
+        System.out.println("Length of string is "+length);
+    }
+}

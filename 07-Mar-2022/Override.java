ae80086372fb7d03b78872a565cc79c0eeb35565
Sanjanadevi-J <100769605+Sanjanadevi-J@users.noreply.github.com>
2022-03-08 13:23:00 +0530

    Interface

--- 07-Mar-2022/Override.java
+++ 07-Mar-2022/Override.java
@@ -1,17 +1,16 @@
 public class Override {
-    void show()
-    {
+    void show() {
         System.out.println("Super class");
     }
-    class OverRide extends Override
-    {
-        void show()
-        {
+
+    class OverRide extends Override {
+        void show() {
             System.out.println("Subclass");
         }
     }
-    public static void main(String[]args) {
-        Override or=new Override();
+
+    public static void main(String[] args) {
+        Override or = new Override();
         or.show();
     }
 }

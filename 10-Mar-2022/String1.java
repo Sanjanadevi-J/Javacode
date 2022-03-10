4207404e25148ac72815f019193dc5d1da0927ff
Sanjanadevi-J <100769605+Sanjanadevi-J@users.noreply.github.com>
2022-03-10 18:53:01 +0530

    String1.java

---
+++ 10-Mar-2022/String1.java
@@ -0,0 +1,11 @@
+public class String1 {
+    public static void main(String[]args)
+    {
+        String s="hello";
+        char[] c= {'w','o','r','l','d'};
+        String s2=new String("java");
+        System.out.println(s);
+        System.out.println(s2);
+        System.out.println(c);
+    }
+}

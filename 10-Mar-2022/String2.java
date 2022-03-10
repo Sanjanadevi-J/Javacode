30595edb84802653c01e4e01d4d021af506a6f87
Sanjanadevi-J <100769605+Sanjanadevi-J@users.noreply.github.com>
2022-03-10 19:00:17 +0530

    Immutable string

---
+++ 10-Mar-2022/String2.java
@@ -0,0 +1,9 @@
+public class String2{
+    public static void main(String[]args){
+        String n="hello";
+        n.append("world");
+        System.out.println(n);
+        String m=n.append("world");
+        System.out.println(m);
+    }
+}

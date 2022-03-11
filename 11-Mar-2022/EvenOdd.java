7e67edd09c90803cad578ca4d26a645d0303fb1a
Sanjanadevi-J <100769605+Sanjanadevi-J@users.noreply.github.com>
2022-03-11 15:34:02 +0530

    Finding odd and even

---
+++ 11-Mar-2022/EvenOdd.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class EvenOdd{
+    public static void main(String args[]){
+        int number=0;
+        Scanner sc = new Scanner(System.in);
+        number= sc.nextInt();
+        String s=Integer.toString(number);
+        char[] ch = new char[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            ch[i] = s.charAt(i);
+        }
+        System.out.print("even:");
+        for(int j=0;j<ch.length;j++) {
+            int num=ch[j];
+            if(j%2==0)
+                System.out.print(c+",");
+        }
+        System.out.print("\b\n");
+        System.out.print("odd:");
+        for(int j=0;j<ch.length;j++) {
+            int num=ch[j];
+            if(c%2!=0){
+                System.out.print(j+",");
+            }
+        }
+        System.out.println("\b");
+
+
+
+    }
+}
